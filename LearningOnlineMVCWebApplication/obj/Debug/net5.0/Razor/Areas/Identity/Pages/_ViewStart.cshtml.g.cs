dde781a7d8a27f939713a316d654393f23e7ff2e
Nikoloz Kavtaradze <nikoloz.kavtaradze8@gmail.com>
2023-08-06 22:57:18 +0400

    Some more files for Login and logout buisness logic

--- LearningOnlineMVCWebApplication/obj/Debug/net5.0/Razor/Areas/Identity/Pages/_ViewStart.cshtml.g.cs
+++ LearningOnlineMVCWebApplication/obj/Debug/net5.0/Razor/Areas/Identity/Pages/_ViewStart.cshtml.g.cs
@@ -12,7 +12,36 @@ namespace AspNetCore
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.Rendering;
     using Microsoft.AspNetCore.Mvc.ViewFeatures;
+#nullable restore
+#line 1 "C:\Users\Kavto\RiderProjects\LearningOnlineMVCWebApplication\LearningOnlineMVCWebApplication\Areas\Identity\Pages\_ViewImports.cshtml"
+using Microsoft.AspNetCore.Identity;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 2 "C:\Users\Kavto\RiderProjects\LearningOnlineMVCWebApplication\LearningOnlineMVCWebApplication\Areas\Identity\Pages\_ViewImports.cshtml"
+using LearningOnlineMVCWebApplication.Areas.Identity;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 3 "C:\Users\Kavto\RiderProjects\LearningOnlineMVCWebApplication\LearningOnlineMVCWebApplication\Areas\Identity\Pages\_ViewImports.cshtml"
+using LearningOnlineMVCWebApplication.Areas.Identity.Pages;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 5 "C:\Users\Kavto\RiderProjects\LearningOnlineMVCWebApplication\LearningOnlineMVCWebApplication\Areas\Identity\Pages\_ViewImports.cshtml"
+using LearningOnlineMVCWebApplication.Data;
+
+#line default
+#line hidden
+#nullable disable
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"431e7f16232e490dfc844b6ad3de4b55f6820593", @"/Areas/Identity/Pages/_ViewStart.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"da59c9981394c389f3e9ffd54167699aa37170c5", @"/Areas/Identity/Pages/_ViewImports.cshtml")]
     public class Areas_Identity_Pages__ViewStart : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #pragma warning disable 1998
