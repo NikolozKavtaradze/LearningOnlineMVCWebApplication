2ab5714766a0b9e207cf76b8fe5f120d567f5d5a
Nikoloz Kavtaradze <nikoloz.kavtaradze8@gmail.com>
2023-08-06 22:55:43 +0400

    Added login model and Login razor page

---
+++ LearningOnlineMVCWebApplication/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using LearningOnlineMVCWebApplication.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(LearningOnlineMVCWebApplication.Areas.Identity.IdentityHostingStartup))]
+namespace LearningOnlineMVCWebApplication.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
